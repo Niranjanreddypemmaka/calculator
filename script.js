f2915fe00de8ce38e400a33061a319a1e09c7088
Niranjanreddypemmaka <pemmakaniranjanreddy@gmail.com>
2021-01-22 10:29:39 +0530

    calculator

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function getHistory() {
     return document.getElementById("history-value").innerText;
 }
 
+
 function printHistory(num) {
     document.getElementById("history-value").innerText = num;
 }
